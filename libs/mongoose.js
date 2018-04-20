2813417bbc28cf0f48689997ff446c68a1938787
v.zinkevichius <v.zinkevichius@betlab.com>
2018-04-20 19:06:19 +0300

    [correct] Fixed config usage for mongoose

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'));
+mongoose.connect(config.mongoose.uri, config.mongoose.options);
 
 module.exports = mongoose;
